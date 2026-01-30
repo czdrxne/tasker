723b31eabb97af25e2281b28fddb642251f411f4
czdrxne <czdrxne@gmail.com>
2026-01-30 17:29:27 +0800

    Development update. Added necessary child components to the main application container.

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import Navigation from "@components/Navigation.svelte";
+</script>
+<div class="w-screen min-h-screen h-screen overflow-y-auto cursor-default bg-l-bg dark:bg-d-bg sm:p-10 p-3">
+    <main class="w-full h-full bg-l-fg dark:bg-d-fg sm:rounded-4xl rounded-2xl shadow-xl sm:py-8 py-5 sm:px-10 px-4">
+        <Navigation />
+    </main>
+</div>
