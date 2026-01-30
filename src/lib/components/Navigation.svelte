915d58e283b5682b3886dd0395d8d17d42042e0e
czdrxne <czdrxne@gmail.com>
2026-01-30 17:27:25 +0800

    Development update. Added components directory and necessary component for the application.

---
+++ src/lib/components/Navigation.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    import star from "@assets/star.svg";
+    import question from "@assets/question.svg";
+
+    import Theme from "@components/Theme.svelte";
+    const actions = [
+        {
+            title: "rate my work!",
+            src: star,
+            href: "",
+            cursor: "cursor-pointer"
+        },
+        {
+            title: "Project Description",
+            src: question,
+            href: "",
+            cursor: "cursor-help"
+        },
+    ];
+</script>
+<nav class="flex justify-between sm:flex-row flex-col-reverse gap-5">
+    <input
+        type="text"
+        placeholder="Search task..."
+        class="focus:outline-[2px] px-5 py-2 rounded-xl sm:w-[45%] focus:outline-acc bg-l-bg dark:bg-d-bg dark:text-white"
+    />
+    <div class="flex items-center sm:gap-5 gap-1">
+        <Theme />
+        {#each actions as action}
+            <a
+                href={action.href}
+                title={action.title}
+                class="bg-l-bg dark:bg-d-bg p-3 rounded-full {action.cursor}"
+            >
+                <img
+                    src={action.src}
+                    class="w-[17px]"
+                    alt={action.alt}
+                />
+            </a>
+        {/each}
+        
+    </div>
+</nav>
---
+++ src/lib/components/Theme.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import toggleLight from "@assets/toggle-light.svg";
+    import toggleDark from "@assets/toggle-dark.svg";
+
+    import { theme } from "@utilities/theme.ts";
+</script>
+<button
+    type="button"
+    aria-label="dark/light mode switch"
+    class="bg-l-bg dark:bg-d-bg p-3 rounded-full cursor-pointer"
+    on:click={() => theme.update(t => t === "dark" ? "light" : "dark")}
+>
+    {#if $theme === "dark"}
+        <img src={toggleDark} alt="toggle theme" class="w-[17px]" />
+    {:else}
+        <img src={toggleLight} alt="toggle theme" class="w-[17px]" />
+    {/if}
+</button>
