b3fe5f678e1e050f5e5ee9dc9b3e6db2c6c0e5b0
czdrxne <czdrxne@gmail.com>
2026-02-03 15:49:44 +0800

    Development update. Created rate link into a component for maintainability.

---
+++ src/lib/components/child/navigation/Rate.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import star from "@assets/star.svg";
+</script>
+
+<a
+    href="https://github.com/czdrxne/tasker/"
+    title="Rate my work!"
+    target="_blank"
+    rel="noreferrer noopener"
+    class="bg-l-bg dark:bg-d-bg sm:p-3 p-[10px] rounded-full cursor-pointer"
+>
+    <img
+        src={star}
+        alt="star"
+        class="sm:w-[17px] w-[20px]"
+    />
+</a>
