e73078549d231b02210364dc2c02768fce0862cf
czdrxne <czdrxne@gmail.com>
2026-02-03 15:53:27 +0800

    Development update. Created a component for delete button that is used in both ongoing and completed sections.

---
+++ src/lib/components/child/content/DeleteAllButton.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { get } from "svelte/store";
+
+    export let store;
+    export let deleteFn         : () => void;
+
+    export let emptyMessage     : string = "Nothing to delete.";
+    export let confirmMessage   : string = "Delete all items?";
+    export let label            : string = "Delete All";
+
+    function onClick() {
+        if (get(store).length === 0) {
+            alert(emptyMessage);
+            return;
+        }
+        if (confirm(confirmMessage)) deleteFn();
+    }
+</script>
+
+<button
+    on:click={onClick}
+    type="button"
+    aria-label={label}
+    class="bg-acc dark:text-d-fg font-semibold text-l-fg px-3 py-[6px] rounded-2xl text-xs cursor-pointer duration-150 active:scale-[0.94] transition-transform ease"
+>
+    {label}
+</button>
